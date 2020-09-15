568dee37c78a3ac59a504f1c5ce2e1ffaa2fcf0c
Jim Harner <jharner@me.com>
2020-09-15 10:33:57 -0400

    updated init.sh

--- rpsql/init.sh
+++ rpsql/init.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
 
-export PATH=/usr/lib/postgresql/12/bin:$PATH
-initdb -D /opt/pg-data
-pg_ctl -D /opt/pg-data start
+PGDATA="/opt/pg-data"
+pgversion="12"
+
+export PATH=/usr/lib/postgresql/${pgversion}/bin:$PATH
+initdb -D $PGDATA
+pg_ctl -D $PGDATA start
 sleep 10
 psql --command "create user rstudio createdb"
 psql --command "create user hive"
+
+# Create databases
 createdb -O rstudio rstudio
 createdb -O hive hive
 createdb -O rstudio dataexpo
 createdb -O rstudio testdb
 createdb -O rstudio nycflights13
+
+# Build databases
 psql -U rstudio dataexpo < /opt/dataexpo.sql  > /dev/null
 gunzip -c /opt/nycflights13.sql.gz | psql -U rstudio nycflights13 > /dev/null
+
+# Grant privileges
+psql --command "GRANT ALL PRIVILEGES ON DATABASE dataexpo TO rstudio"
+psql --command "GRANT ALL PRIVILEGES ON DATABASE nycflights13 TO rstudio"
+psql --command "GRANT ALL PRIVILEGES ON DATABASE testdb TO rstudio"
 service postgresql stop
 
