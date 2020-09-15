f13ddeacb64716b544472d4378556b4862487901
Chris Grant <chrisgrant.datascience@gmail.com>
2020-09-15 17:29:54 -0400

    Removed commented out code.

--- rpsql/init.sh
+++ rpsql/init.sh
@@ -15,19 +15,6 @@ chown postgres /opt/pg-data
 ls -ld $PGDATA
 
 export PATH=/usr/lib/postgresql/12/bin:$PATH
-#initdb -D /opt/pg-data
-#pg_ctl -D /opt/pg-data start
-#sleep 10
-#psql --command "create user rstudio createdb"
-#psql --command "create user hive"
-#createdb -O rstudio rstudio
-#createdb -O hive hive
-#createdb -O rstudio dataexpo
-#createdb -O rstudio testdb
-#createdb -O rstudio nycflights13
-#psql -U rstudio dataexpo < /opt/dataexpo.sql  > /dev/null
-#gunzip -c /opt/nycflights13.sql.gz | psql -U rstudio nycflights13 > /dev/null
-#service postgresql stop
 
 echo "$PGDATA does not exist. creating database."
 ${appdir}/initdb -D $PGDATA
